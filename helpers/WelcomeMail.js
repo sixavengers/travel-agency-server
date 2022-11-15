34b645b09a3412b13c5ede72f2167934d05b7329
saymanrabbi <mdrabbi.asm@gmail.com>
2022-11-15 12:46:05 +0600

    changes color

--- helpers/WelcomeMail.js
+++ helpers/WelcomeMail.js
@@ -81,7 +81,7 @@ module.exports.adminMail= async (name,email,url,sub)=>{
         from: process.env.EMAIL,
         to: email,
         subject: sub,
-        html: `<div style="max-width:700px;margin-bottom:1rem;display:flex;align-items:center;gap:10px;font-family:Roboto;font-weight:600;color:#3b5998"><img src="https://res.cloudinary.com/dnr5u3jpb/image/upload/v1667486487/logo_lpnpjj.png" alt="" style="width:30px"><span style="text:red;font-weight: bold;">please approve my package</span></div><div style="padding:1rem 0;border-top:1px solid #e5e5e5;border-bottom:1px solid #e5e5e5;color:#141823;font-size:17px;font-family:Roboto"><span>Hello ${name}</span><div style="padding:20px 0"><span style="padding:1.5rem 0">package info url</span></div><a href=${url} style="width:200px;padding:10px 15px;background:#4c649b;color:#fff;text-decoration:none;font-weight:600">Confirm Package</a><br><div style="padding-top:20px"><span style="margin:1.5rem 0;color:#898f9c">Powered By Travel Agency</span></div></div>`,
+        html: `<div style="max-width:700px;margin-bottom:1rem;display:flex;align-items:center;gap:10px;font-family:Roboto;font-weight:600;color:#3b5998"><img src="https://res.cloudinary.com/dnr5u3jpb/image/upload/v1667486487/logo_lpnpjj.png" alt="" style="width:30px"><span style="color:red;font-weight: bold;">please approve my package</span></div><div style="padding:1rem 0;border-top:1px solid #e5e5e5;border-bottom:1px solid #e5e5e5;color:#141823;font-size:17px;font-family:Roboto"><span>Hello ${name}</span><div style="padding:20px 0"><span style="padding:1.5rem 0">package info url</span></div><a href=${url} style="width:200px;padding:10px 15px;background:#4c649b;color:#fff;text-decoration:none;font-weight:600">Confirm Package</a><br><div style="padding-top:20px"><span style="margin:1.5rem 0;color:#898f9c">Powered By Travel Agency</span></div></div>`,
     }
     let info = await transporter.sendMail(mailOptions);
     return info;
@@ -104,7 +104,7 @@ module.exports.serviceEmail= async (email,text,sub)=>{
         from: process.env.EMAIL,
         to: email,
         subject: sub,
-        html: `<h2 style="text:red;font-weight:bold">${text}</h2>`,
+        html: `<h2 style="color:red;font-weight:bold">${text}</h2>`,
     }
     let info = await transporter.sendMail(mailOptions);
     return info;
