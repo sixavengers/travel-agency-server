f2d7bf5bcfb6df46fe0c1aa386b9a8837791920a
saymanrabbi <mdrabbi.asm@gmail.com>
2022-11-15 18:42:49 +0600

    bookedservices

--- controllers/payment.controler.js
+++ controllers/payment.controler.js
@@ -1,6 +1,13 @@
+const User = require('../Models/User');
+
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const createPaymentIntent = async (req, res) => {
       try {
+        const id = req.userData.id
+        const user = await User.findById(id);
+        if(user.role!=="user"){
+            return res.status(400).json({ messages: "You are not a user" });
+        }
         const {  price} = req.body;
         const amount = price * 100;
         const paymentIntent = await stripe.paymentIntents.create({
